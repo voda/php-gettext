ee989d7aff5efcafd6218af5af7affeea7922063
Danilo Šegan <danilo@canonical.com>
2009-12-02 01:49:27 +0100

    More improvements to the locale list function.

--- tests/LocalesTest.php
+++ tests/LocalesTest.php
@@ -27,12 +27,16 @@ class LocaleTest extends PHPUnit_Framework_TestCase
     // For a locale containing country code, we prefer
     // full locale name, but if that's not found, fall back
     // to the language only locale name.
-    $this->assertEquals(array("sr_RS.", "sr_RS", "sr"),
+    $this->assertEquals(array("sr_RS", "sr"),
                         get_list_of_locales("sr_RS"));
 
     // If language code is used, it's the only thing returned.
-    $this->assertEquals(array("sr_.", "sr_", "sr"),
+    $this->assertEquals(array("sr"),
                         get_list_of_locales("sr"));
+
+    // There is support for language and charset only.
+    $this->assertEquals(array("sr.UTF-8", "sr"),
+                        get_list_of_locales("sr.UTF-8"));
 
     // It can also split out character set from the full locale name.
     $this->assertEquals(array("sr_RS.UTF-8", "sr_RS", "sr"),
@@ -43,13 +47,18 @@ class LocaleTest extends PHPUnit_Framework_TestCase
                               "sr_RS.UTF-8", "sr_RS", "sr"),
                         get_list_of_locales("sr_RS.UTF-8@latin"));
 
+    // We can pass in only language and modifier.
+    $this->assertEquals(array("sr@latin", "sr"),
+                        get_list_of_locales("sr@latin"));
+
+
     // If locale name is not following the regular POSIX pattern,
     // it's used verbatim.
     $this->assertEquals(array("something"),
                         get_list_of_locales("something"));
 
     // Passing in an empty string returns an empty array.
-    $this->assertEquals(array(""),
+    $this->assertEquals(array(),
                         get_list_of_locales(""));
   }
 }
